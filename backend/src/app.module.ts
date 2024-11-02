2e0d0ad51d4ce875f3108d5fe0d919530a5d483b
iti-rina <itigecheva.irina@gmail.com>
2024-11-02 15:51:57 +0700

    Create basic structure for polls module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PollsModule } from './polls/polls.module';
 
 @Module({
-  imports: [],
+  imports: [PollsModule],
   controllers: [AppController],
   providers: [AppService],
 })
