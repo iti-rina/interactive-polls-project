202ceee999183411215d5b7825b3ba353b1cadbd
iti-rina <itigecheva.irina@gmail.com>
2024-11-02 16:17:39 +0700

    feat: add basic logic for routes in controller

--- backend/src/polls/polls.controller.ts
+++ backend/src/polls/polls.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('polls')
-export class PollsController {}
+export class PollsController {
+  // GET /api/polls - Получить список всех опросов.
+  @Get()
+  findAll() {
+    return []
+  }
+
+  // POST /api/polls - Создать новый опрос.
+  @Post()
+  create(@Body() poll: {}) {
+    return poll
+  }
+
+  // POST /api/polls/:id/vote - Проголосовать за один из вариантов в опросе по его ID.
+  @Post(':id/vote')
+  vote(@Body() vote: {}) {
+    return vote
+  }
+
+  // DELETE /api/polls/:id - Удалить опрос по его ID.
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id
+  }
+}
