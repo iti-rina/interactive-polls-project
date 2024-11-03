8819c544e089ce6a957e475d7ab4911d10f58787
iti-rina <itigecheva.irina@gmail.com>
2024-11-03 15:16:46 +0700

    feat: create PrismaModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PollsModule } from './polls/polls.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PollsModule],
+  imports: [PollsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/polls/polls.module.ts
+++ backend/src/polls/polls.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [PollsController],
-  providers: [PollsService]
+  providers: [PollsService],
+  imports: [PrismaModule],
 })
 export class PollsModule {}
