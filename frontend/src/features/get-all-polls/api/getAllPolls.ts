7ea84aa723120a30e9016618cad6640f0001ed1e
iti-rina <itigecheva.irina@gmail.com>
2024-11-04 17:22:35 +0700

    fix: modify request function for more convenient data

--- frontend/src/features/get-all-polls/api/getAllPolls.ts
+++ frontend/src/features/get-all-polls/api/getAllPolls.ts
@@ -4,7 +4,8 @@ import type { PollValues } from '../../../shared';
 export default async function getAllPolls(): Promise<PollValues[]> {
   const API_BASE_URL = import.meta.env.VITE_API_URL;
   try {
-    return await axios.get(API_BASE_URL);
+    const response = await axios.get(API_BASE_URL)
+    return response.data;
   } catch(error) {
     console.error(error);
     throw error;
