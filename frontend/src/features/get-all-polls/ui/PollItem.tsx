f90f054bece2c4767e945a7aa4b2e5f562761f40
iti-rina <itigecheva.irina@gmail.com>
2024-11-04 18:26:08 +0700

    feat: add VoteModal to PollItem component

--- frontend/src/features/get-all-polls/ui/PollItem.tsx
+++ frontend/src/features/get-all-polls/ui/PollItem.tsx
@@ -2,13 +2,29 @@ import { FC, useState } from 'react';
 import { List, Button } from 'antd';
 import { SelectOutlined } from '@ant-design/icons';
 import type { PollValues } from '../../../shared';
-
+import { sendVote } from '../../vote/api';
+import { VoteModal } from '../../vote/ui';
 
 type PollItemValues = {
   pollData: PollValues;
 }
 
 const PollItem:FC<PollItemValues> = ({ pollData }) => {
+  const [voteFormOpen, setVoteFormOpen] = useState(false);
+  
+  const handleClickOnItem = () => {
+    setVoteFormOpen(true);
+  }
+
+  const handleCancel = () => {
+    setVoteFormOpen(false);
+  };
+
+  const handleVote = (pollId: number, answerId: number) => {
+    sendVote(pollId, answerId, 'INCREMENT');
+    setVoteFormOpen(false);
+  };
+
   let stringVotesCount = '';
   if (pollData.total_votes === 0) {
     stringVotesCount = 'Your vote will be the first';
@@ -22,12 +38,19 @@ const PollItem:FC<PollItemValues> = ({ pollData }) => {
     <>
       <List.Item
         actions={[<Button ><SelectOutlined /></Button>]}
+        onClick={handleClickOnItem}
       >
         <List.Item.Meta 
           title={pollData.text}
           description={stringVotesCount}
         />
       </List.Item>
+      <VoteModal
+        visible={voteFormOpen}
+        onCancel={handleCancel}
+        onVote={handleVote}
+        pollData={pollData}
+      />
     </>
   );
 }
