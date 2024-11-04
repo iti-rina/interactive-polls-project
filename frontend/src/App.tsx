038ed5bd249b05e79e96a244430dce34c1e01088
iti-rina <itigecheva.irina@gmail.com>
2024-11-05 00:20:02 +0700

    feat: create App component with QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,33 @@
-import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { CreatePollComponent } from './features/create-poll/ui';
+import { FloatButton } from 'antd';
+import { FormOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { PollList } from './features/get-all-polls/ui';
 
-function App() {
+const queryClient = new QueryClient();
+
+const App = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const handleOpenModal = () => {
+    setModalOpen(prev => !prev)
+  }
+
+  const handleCancel = () => {
+    setModalOpen(false);
+  }
+
   return (
-    <>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <PollList />
+      <FloatButton 
+        tooltip={<div>Create new poll</div>} 
+        icon={<FormOutlined />}
+        onClick={handleOpenModal}
+      />
+      <CreatePollComponent isOpen={isModalOpen} onCancel={handleCancel}/>
+    </QueryClientProvider>
+  );
 }
 
 export default App
