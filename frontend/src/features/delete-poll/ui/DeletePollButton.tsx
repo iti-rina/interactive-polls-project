00d0afcc7a502e79a0704649fd9673443bd70e5b
iti-rina <itigecheva.irina@gmail.com>
2024-11-05 00:26:09 +0700

    feat: update component to use React-Query

--- frontend/src/features/delete-poll/ui/DeletePollButton.tsx
+++ frontend/src/features/delete-poll/ui/DeletePollButton.tsx
@@ -2,15 +2,24 @@ import { Button, Popconfirm } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
 import { deletePoll } from '../api';
 import { FC } from 'react';
+import { useQueryClient, useMutation } from 'react-query';
 
 type DeletePollButtonProps = {
   id: number
 }
 
 const DeletePollButton:FC<DeletePollButtonProps> = ({ id }) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(() => deletePoll(id), {
+    onSuccess: () => {
+      queryClient.invalidateQueries('polls');
+    },
+  });
+
   const handleDelete = () => {
-    deletePoll(id)
-  }
+    mutation.mutate();
+  };
 
   return (
     <Popconfirm
