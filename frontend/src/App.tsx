f6177f314e5f4629b4890209fb6207f72d5251fe
iti-rina <itigecheva.irina@gmail.com>
2024-11-05 03:10:51 +0700

    feat(styles): add styles for app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { CreatePollComponent } from './features/create-poll/ui';
-import { FloatButton } from 'antd';
-import { FormOutlined } from '@ant-design/icons';
+import { FloatButton, Typography } from 'antd';
+import { EditTwoTone, SmileOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 import { PollList } from './features/get-all-polls/ui';
+import styles from './App.module.css'
 
 const queryClient = new QueryClient();
 
@@ -19,13 +20,17 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <PollList />
-      <FloatButton 
-        tooltip={<div>Create new poll</div>} 
-        icon={<FormOutlined />}
-        onClick={handleOpenModal}
-      />
-      <CreatePollComponent isOpen={isModalOpen} onCancel={handleCancel}/>
+      <div className={styles.wrapper}>
+        <Typography.Title mark>Polls: Create Your Own or Share Your Opinion on Existing Questions <SmileOutlined /></Typography.Title>
+        <PollList />
+        <FloatButton 
+          tooltip={<div>Create new poll</div>} 
+          icon={<EditTwoTone twoToneColor='#ff8127'/>}
+          onClick={handleOpenModal}
+          className={styles.floatBtn}
+        />
+        <CreatePollComponent isOpen={isModalOpen} onCancel={handleCancel}/>
+      </div>
     </QueryClientProvider>
   );
 }
