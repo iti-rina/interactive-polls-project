653028e4729ec5e7d98cebd5721ccdf796c9c677
iti-rina <itigecheva.irina@gmail.com>
2024-11-04 17:21:52 +0700

    fix: add correct type

--- frontend/src/features/get-all-polls/api/getAllPolls.ts
+++ frontend/src/features/get-all-polls/api/getAllPolls.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import type { PollValues } from '../../../shared';
 
-
-export default async function getAllPolls(): Promise<PollValues> {
+export default async function getAllPolls(): Promise<PollValues[]> {
   const API_BASE_URL = import.meta.env.VITE_API_URL;
   try {
     return await axios.get(API_BASE_URL);
